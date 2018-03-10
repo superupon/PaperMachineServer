4a47250ca894b118b8c6207435f35b72e02efc81
superupon <superupon@126.com>
2018-03-10 10:49:09 +0800

    add timer to hold TCP connection

--- app.js
+++ app.js
@@ -38,18 +38,30 @@ net.createServer(function(sock){
     result = data.toString().match(pattern)
     if(result != null)
     {
-       if(!(result[1] in socket_list))
+       //if(!(result[1] in socket_list))
+       //if(typeof(socket) != 'string')
        {
           socket_list[result[1]] = sock
+          //console.log('socket get: ' + socket +'' + typeof(socket))
+          //socket_list[result[1]].write('TWO')
+          //console.log('write two ' + result[1])
        }
     }
   })
+  sock.on('error', (err) =>
+    //console.log('Caught socket error')
+    console.log(err.stack)
+  )
 }).listen(PORT, HOST)
 
-//var timer = setInterval(log, 10000)
+var timer = setInterval(log, 30000)
 function log()
 {
   console.log('timer');
-  if(socket)
-    socket.write('ONE');
+  console.log('socket_list length: ' + socket_list.length)
+  for (socket in socket_list)
+  {
+    socket_list[socket].write('TWO')
+    console.log("socket number: " + socket)
+  }
 }
