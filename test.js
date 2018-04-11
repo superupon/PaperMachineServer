d186c4f273055e9cec940f90d6e1a604c86d13c6
superupon <superupon@126.com>
2018-04-11 09:30:38 +0800

    Add sql handle for select wx_id count

--- test.js
+++ test.js
@@ -1,12 +1,39 @@
-qcloud = require('../qcloud')
+qcloud = require('./qcloud')
 const { mysql } = qcloud
-var blacklist = setInterval(detectBlackList, 10000)
+var blacklist = setInterval(detectBlackList, 1000)
+//mysql.schema.createTable('blacklist', function(table) {
+//   table.increments();
+//   table.string('name')
+//}).then(res=>{console.log(res)})
+//mysql('blacklist').insert({name : 'test'}).returning('*').then(res=> {console.log(res)})
+mysql('blacklist').select('*').returning('*').then(res=> {console.log(res)})
 
 function detectBlackList()
 {
     currentTime = new Date()
-    minutes = 3
-    targetTime = currentTime - minutes * 1000 * 60 
+    minutes = 4 
+    //targetTime = currentTime - minutes * 1000 * 60 
+    targetTime = currentTime - minutes * 1000 * 60 * 60
     console.log("HaHa")
-    mysql('request').select('wx_id').where('time' > targetTime).returning('*').then(res => { console.log(res) })
+    date = new Date(targetTime)
+    console.log(date)
+    users = []
+    test = {}
+    //mysql('request').select('wx_id').where('time' > targetTime).returning('*').then(res => { console.log(res) })
+    mysql('request')
+    .select('wx_id')
+    .where('time','>', date).groupBy('wx_id')
+    .returning('*')
+    .then(res => { 
+                    console.log(res)
+                    for (var temp in res){
+                        console.log(res[temp]['wx_id'])
+                        mysql('request').select('wx_id').where('wx_id', res[temp]['wx_id']).count('wx_id as cnt').returning('*')
+                        .then(
+                        rs => {
+                            console.log(rs)
+                        })
+                    }
+                 })
+    console.log(users)
 }
