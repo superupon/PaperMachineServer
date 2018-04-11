8fdde6436e3339765b9fa3f2b5681ab332fba5ca
superupon <superupon@126.com>
2018-04-11 13:58:27 +0800

    Fix bugs for adding wrong names to blacklist

--- test.js
+++ test.js
@@ -13,7 +13,7 @@ mysql('blacklist').select('*').returning('*').then(res=> {console.log(res)})
 function detectBlackList() {
     currentTime = new Date()
     minutes = 4
-    tapCnt = 10
+    tapCnt = 4
     //targetTime = currentTime - minutes * 1000 * 60 
     targetTime = currentTime - minutes * 1000 * 60 * 60
     console.log("HaHa")
@@ -24,28 +24,30 @@ function detectBlackList() {
     //mysql('request').select('wx_id').where('time' > targetTime).returning('*').then(res => { console.log(res) })
     mysql('request').select('wx_id').where('time', '>', date).groupBy('wx_id').returning('*')
         .then(res => {
-            console.log(res)
+            //console.log(res)
             for (var temp in res) {
-                console.log(res[temp]['wx_id'])
+                //console.log(res[temp]['wx_id'])
                 mysql('request').select('wx_id').where('wx_id', res[temp]['wx_id']).count('wx_id as cnt').returning('*')
                     .then(rs => {
                             for (var record in rs){
-                                console.log(rs[record]['cnt'])
+                                //console.log(rs[record]['cnt'])
                                 if(rs[record]['cnt'] > tapCnt){
-                                    mysql('blacklist').insert({name : res[temp]['wx_id']}).returning('*')
+                                    console.log('add into blacklist')
+                                    mysql('blacklist').insert({name : rs[record]['wx_id']}).returning('*').then(res=>{console.log(res)})
                                 }
                             }
                             console.log(rs)
                         })
             }
         })
-    console.log(users)
+    //console.log(users)
 }
 
 function loadBlacklist()
 {
     mysql('blacklist').select('name').returning('*').then(
         res => { BlackList = res
-        console.log(res)}
+        console.log('blacklist')
+        console.log(BlackList)}
     )
 }
