314a7e38ce4965481c10b34e69f3e8dbfdd71b81
Yuchen Zhang <yuchen.zhang@ericsson.com>
2018-05-10 15:14:58 +0800

    Add create table function and create first account function

--- sql.js
+++ sql.js
@@ -1,3 +1,16 @@
+// helpful script to support databse creation
 qcloud = require('./qcloud')
 const { mysql } = qcloud
-mysql('')
+function createAdmin() {
+    mysql.schema.createTable('admin', function (table) {
+        table.string('name')
+        table.string('password')
+        table.timestamps()
+    })
+}
+
+function insertRootAccount() {
+    mysql('admin')
+        .insert({ name: 'root', password: 'root' })
+        .then(res => { console.log(res) })
+}
