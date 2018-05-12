de234407f1c1ed0c874445df8979b2452e7b106d
Yuchen Zhang <superupon@126.com>
2018-05-12 12:39:04 +0800

    Fix wrong router path

--- routes/index.js
+++ routes/index.js
@@ -35,9 +35,9 @@ router.post('/message', controllers.message.post)
 router.get('/request', controllers.request.get)
 router.post('/request', controllers.request.post)
 
-router.get('/back_login', controllers.request.get)
-router.post('/back_login', controllers.tunnel.post)
+router.get('/back_login', controllers.back_login.get)
+router.post('/back_login', controllers.back_login.post)
 
-router.get('/data', controllers.request.get)
-router.post('/data', controllers.tunnel.post)
+router.get('/data', controllers.data.get)
+router.post('/data', controllers.data.post)
 module.exports = router
