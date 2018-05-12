369334265c32567f1138b790e6c5712ce00f9862
superupon <superupon@126.com>
2018-05-12 16:19:37 +0800

    Use await and async to define this function and Add Unit test cases

--- database.js
+++ database.js
@@ -2,14 +2,32 @@ qcloud = require('./qcloud')
 const { mysql } = qcloud
 var database = {}
 
-database.isAdminUser = function(username, pswd)
-{
-    mysql('admin')
-    .select({name : username, password : pswd})
-    .returnning('*')
+database.isAdminUser = async function(username, pswd) {
+    ret = 0
+    value = []
+    await mysql('admin')
+    .select('*')
+    .where('name', username)
+    .returning('*')
     .then(res => {
-        console.log(username + pswd)
-        console.log(res)})
+        ret = res.length
+        value = res})
+    if(ret == 0)
+        return false
+    else {
+        if (value[0]['password'] == pswd) 
+            return true
+        else
+            return false
+    }
+}
+
+database.Test = async function() {
+    result = await database.isAdminUser('hi', 'hi')
+    console.log(result)
+    result = await database.isAdminUser('root', 'root')
+    console.log(result)
 }
 
 module.exports = database
+database.Test()
