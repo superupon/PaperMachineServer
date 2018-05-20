e1ba64ffb3eed6ab7abdb017560a855d662d1cef
superupon <superupon@126.com>
2018-05-20 16:24:57 +0800

    Add interface for getDeviceRecord, getTotalRecordNumberForDevice, getTotalRecordNumberForDeviceOneDate in database module

--- database.js
+++ database.js
@@ -34,10 +34,18 @@ database.getBlacklistUser = async function(ofst) {
     return ret
 }
 
+database.deleteBlacklistUser = async function(number) {
+    ret = []
+    await mysql('blacklist')
+    .where('id', number)
+    .del()
+}
+
 database.getDevices = async function(ofst) {
     ret = []
     await mysql('devices')
     .select('*')
+    .orderBy('card_id', 'asc')
     .limit(10)
     .offset(ofst)
     .then(res => {
@@ -68,6 +76,47 @@ database.deleteDevice = async function(deviceCardId) {
     .where('card_id', deviceCardId)
     .del()
 }
+
+database.getDeviceRecord = async function(deviceCardId, ofst) {
+    ret = []
+    await mysql('request')
+    .select('*')
+    .where('device_id', deviceCardId)
+    .limit(10)
+    .offset(ofst)
+    .orderBy('time','desc')
+    .then(res => {
+        ret = res
+    })
+    return ret
+}
+
+database.getTotalRecordNumberForDevice = async function(deviceCardId) {
+    ret = 0
+    await mysql('request')
+    .select('*')
+    .where('device_id', deviceCardId)
+    .count('device_id as cnt')
+    .then(res => {
+        ret = res[0].cnt
+    })
+    return ret
+}
+
+database.getTotalRecordNumberForDeviceOneDate = async function(deviceCardId, date) {
+    ret = 0
+    date = date - (date % 86400000)
+    await mysql('request')
+    .select('*')
+    .where('device_id', deviceCardId)
+    .where('time', '>' , new Date(date))
+    .count('device_id as cnt')
+    .then(res => {
+        ret = res[0].cnt
+    })
+    return ret
+}
+
 
 database.Test = async function() {
     console.log('---------------isAdminUser--------')
@@ -107,7 +156,24 @@ database.Test = async function() {
     await database.deleteDevice('')
     result = await database.getDevices(0)
     console.log(result)
+
+    console.log('---------------getDeviceRecord-----')
+    result = await database.getDeviceRecord('460068111016038', 0)
+    console.log(result)
+    result = await database.getDeviceRecord('460043906007803', 0)
+    console.log(result)
+
+    console.log('---------------getTotalRecordNumberForDevice-------')
+    result = await database.getTotalRecordNumberForDevice('460068111016038')
+    console.log('460068111016038 total number: ' + result)
+    result = await database.getTotalRecordNumberForDevice('460043906007809')
+    console.log('460043906007809 total number: ' + result)
+
+    console.log('---------------getTotalRecordNumberForDeviceOneDate-------')
+    result = await database.getTotalRecordNumberForDeviceOneDate('460068111016038', new Date())
+    console.log('460068111016038 total number: ' + result)
+    result = await database.getTotalRecordNumberForDeviceOneDate('460043906007809', new Date())
+    console.log('460043906007809 total number: ' + result)
 }
 
 module.exports = database
-//database.Test()
