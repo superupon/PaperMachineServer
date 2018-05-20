f608e622055cf0b2716971523dda86b8f0058e1d
superupon <superupon@126.com>
2018-05-20 16:22:27 +0800

    Add delete blacklist, get logging data, and delete device function

--- controllers/data.js
+++ controllers/data.js
@@ -1,5 +1,6 @@
 var database = require('../database')
 var socket_list = require('./request').socket_list
+var socket_state = require('./request').socket_state
 async function getNewDevices() {
     var result = []
     console.log(socket_list)
@@ -7,7 +8,7 @@ async function getNewDevices() {
     console.log(typeof(result))
     for(var socket in socket_list)
     {
-       if(socket_list[socket].writable)
+       if(socket_list[socket].writable && socket_state[socket])
        { 
           console.log('socket' + socket)
           var has_device = await database.hasDevice(socket)
@@ -26,8 +27,12 @@ async function get (ctx, next) {
     console.log('data get')
     if ( ctx.query['type'] == 2)
         ret = await database.getBlacklistUser(parseInt(ctx.query['offset']))
-    else if (ctx.query['type'] == 1)
+    else if (ctx.query['type'] == 1) {
         ret = await database.getDevices(parseInt(ctx.query['offset']))
+        for(var temp in ret) {
+            ret[temp].state = socket_state[ret[temp].card_id]
+        }
+    }
     else if (ctx.query['type'] == 3){
         if(await database.hasDevice(ctx.query['card_id'])){
             await database.deleteDevice(ctx.query['card_id'])
@@ -39,6 +44,23 @@ async function get (ctx, next) {
     else if (ctx.query['type'] == 4) {
         ret = await getNewDevices()
         console.log(ret)
+    }
+    else if (ctx.query['type'] == 5) {
+        console.log('delte blacklist')
+        ret = await database.deleteBlacklistUser(parseInt(ctx.query['id']))
+    }
+    else if (ctx.query['type'] == 6) {
+        response = {}
+        result = await database.getDeviceRecord(ctx.query['card_id'], parseInt(ctx.query['offset']))
+        response['records'] = result
+        result = await database.getTotalRecordNumberForDeviceOneDate(ctx.query['card_id'], new Date())
+        response['totalToday'] = result
+        result = await database.getTotalRecordNumberForDevice(ctx.query['card_id'])
+        response['total'] = result
+        ret = response
+    }
+    else if (ctx.query['type'] == 7) {
+        await database.deleteDevice(ctx.query['card_id'])
     }
     ctx.body = ret
 }
